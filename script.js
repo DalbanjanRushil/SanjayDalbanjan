5a7455f7bb27abc62cddefbe4d21a6e7bd3f390e
DalbanjanRushil <dalbanjanrushil0@gmail.com>
2025-07-12 21:30:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ setTimeout(() => {
   document.querySelector('.loader').style.display = 'none';
   document.querySelector('.main-content').style.display = 'block';
 }, 3000);
+
+const text = "અહી તમે કામદાર નથી, પરિવારનો હિસ્સો છો.";
+const speed = 70;
+let i = 0;
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("typewriter-text").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+setTimeout(() => {
+  document.querySelector('.loader').style.display = 'none';
+  document.querySelector('.main-content').style.display = 'block';
+  typeWriter(); // Start typing after loader
+}, 3000);
+
